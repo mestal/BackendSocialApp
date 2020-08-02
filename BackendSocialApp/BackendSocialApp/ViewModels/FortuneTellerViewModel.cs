69579accad61a5876527b6285c08c9085c4f1c69
Mesut Alpaslan <mestal@yahoo.com>
2020-08-02 10:00:12 +0300

    FortuneTellerViewModel - FullName

--- BackendSocialApp/BackendSocialApp/ViewModels/FortuneTellerViewModel.cs
+++ BackendSocialApp/BackendSocialApp/ViewModels/FortuneTellerViewModel.cs
@@ -10,7 +10,7 @@ namespace BackendSocialApp.ViewModels
     {
         public Guid Id { get; set; }
 
-        public string Name { get; set; }
+        public string FullName { get; set; }
 
         public string ConnectionStatus { get; set; }
 
